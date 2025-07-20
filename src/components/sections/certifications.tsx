947d9054f2bac71f3736ec975adf4190faaaf3b5
Vikas Ravikumar Karjigi <vikasravikarjigi26@gmail.com>
2025-07-20 01:02:47 +0000

    then add all the things present in this github link https://github.com/v

--- src/components/sections/certifications.tsx
+++ src/components/sections/certifications.tsx
@@ -2,7 +2,63 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Award } from "lucide-react"
 import Link from "next/link"
 
-const certificationsData: { name: string; issuer: string; date: string; link: string }[] = []
+const certificationsData = [
+  {
+    name: "Certified Kubernetes Application Developer (CKAD)",
+    issuer: "The Linux Foundation",
+    date: "Issued Nov 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/01_CKAD_certificate.pdf",
+  },
+  {
+    name: "Meta Database Engineer Professional Certificate",
+    issuer: "Meta",
+    date: "Issued Oct 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/02_Meta_Database_Engineer_Professional_Certificate.pdf",
+  },
+  {
+    name: "AWS Certified Machine Learning - Specialty",
+    issuer: "Amazon Web Services (AWS)",
+    date: "Issued Sep 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/03_AWS_Certified_Machine_Learning_Specialty_certificate.pdf",
+  },
+  {
+    name: "AWS Certified Cloud Practitioner",
+    issuer: "Amazon Web Services (AWS)",
+    date: "Issued Aug 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/04_AWS_Certified_Cloud_Practitioner_certificate.pdf",
+  },
+  {
+    name: "TensorFlow Developer Certificate",
+    issuer: "Google",
+    date: "Issued Jul 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/05_TensorFlow_Developer_Certificate.pdf",
+  },
+  {
+    name: "Meta Version Control",
+    issuer: "Meta",
+    date: "Issued Jun 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/06_Meta_Version_Control_certificate.pdf",
+  },
+  {
+    name: "Google Cloud Digital Leader",
+    issuer: "Google",
+    date: "Issued May 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/07_Google_Cloud_Digital_Leader_Certificate.pdf",
+  },
+  {
+    name: "AWS Academy Graduate - AWS Academy Cloud Foundations",
+    issuer: "Amazon Web Services (AWS)",
+    date: "Issued Apr 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/08_AWS_Academy_Graduate_AWS_Academy_Cloud_Foundations.pdf",
+  },
+  {
+    name: "Introduction to Cybersecurity",
+    issuer: "Cisco",
+    date: "Issued Mar 2024",
+    link: "https://raw.githubusercontent.com/vikasrkarjigi/karjigi_portfolio/main/certifications/09_Introduction_to_Cybersecurity_Cisco.pdf",
+  },
+];
+
 
 export function Certifications() {
   if (certificationsData.length === 0) {
